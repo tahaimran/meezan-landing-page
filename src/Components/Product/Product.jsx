53b08a10e2f44ff6c1d6722934d6c683e023c87a
tahaimran <tahaimran.24@gmail.com>
2022-07-06 14:25:25 +0500

    scroll added to products

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -4,10 +4,11 @@ import { CardGroup,Card } from 'react-bootstrap';
 
 function Product() {
   return (
-    <div className='container product'>
+    <div className=' product'>
       <h1 className='text-center'>Products</h1>
-      <CardGroup>
-        <Card className='card'>
+      <div className='prodGrp'>
+      <div className='cardGroup '>
+        <Card className='card '>
           <Card.Img variant="top"   className='prodImg' src="http://nayabox.com/api/web/upload/6FM200.jpg" />
           <Card.Body className='cardBody'>
             <Card.Title className='prodTitle'>Card title</Card.Title>
@@ -41,7 +42,32 @@ function Product() {
           </Card.Body>
          
         </Card>
-      </CardGroup>
+        <Card className='card'>
+          <Card.Img variant="top" className='prodImg' src="http://nayabox.com/api/web/upload/6FM200.jpg" />
+          <Card.Body className='cardBody'>
+            <Card.Title className='prodTitle'>Card title</Card.Title>
+            <Card.Text className='prodPara'>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to
+              show that equal height action.
+            </Card.Text>
+          </Card.Body>
+         
+        </Card>
+        <Card className='card'>
+          <Card.Img variant="top" className='prodImg' src="http://nayabox.com/api/web/upload/6FM200.jpg" />
+          <Card.Body className='cardBody'>
+            <Card.Title className='prodTitle'>Card title</Card.Title>
+            <Card.Text className='prodPara'>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to
+              show that equal height action.
+            </Card.Text>
+          </Card.Body>
+         
+        </Card>
+      </div>
+      </div>
     </div>
   )
 }
